1b796527c951b4821d9ba9d8547e392223e4bcf0
Sam <samara.amanda01@gmail.com>
2021-06-20 22:01:15 -0400

    removedd testing code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
 class App extends React.Component {
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/users/1/orders')
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-  }
+
+  
 
   render() {
     return (
