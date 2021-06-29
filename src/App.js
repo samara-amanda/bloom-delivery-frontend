1f47c6cffcee25905f5236e12a6d27ab0bea9eb3
Sam <samara.amanda01@gmail.com>
2021-06-28 23:04:46 -0400

    added components and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import Login from './components/Login.js'
+import './App.css'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
+import NavBar from './components/NavBar.js'
+import Login from './components/Login.js'
+import Signup from './components/Signup.js';
+import { BrowserRouter, Switch } from 'react-router-dom'
+import { Route } from 'react-router'
+
+import MyOrders from './components/MyOrders.js';
 
 class App extends React.Component {
 
@@ -10,11 +17,33 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <Login/>
-    );
+      return (
+      
+        <div className="App">
+          <BrowserRouter>
+          <NavBar/>
 
+            <Switch>
+              
+              
+              <Route exact path='/signup' component={Signup}/>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/my-orders' component={MyOrders}/>
+            </Switch>
+          </BrowserRouter>
+        </div>
+      );
   }
 }
 
-export default connect(null, { getCurrentUser })(App);
+//I do this because I know the incoming argument is an object, state, coming from redux
+// and I know it has a property called currentUser
+//state = { ...
+//  currentUser: {...}
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
