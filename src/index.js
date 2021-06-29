dc503ef884f48e3beecbe098eda6345f93080f6e
Sam <samara.amanda01@gmail.com>
2021-06-28 22:58:11 -0400

    cleaned up code relocated store

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import usersReducer from './reducers/usersReducer.js'
-import currentUser from './reducers/currentUser.js'
-import loginForm from './reducers/loginForm.js'
+import store from './store.js'
 
-
-
-const reducer = combineReducers({
-  users: usersReducer, 
-  currentUser, 
-  loginForm
-})
-
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-let store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
