8d158cb0ded57244d03f10985e3272e05a748e99
Sam <samara.amanda01@gmail.com>
2021-06-28 23:03:47 -0400

    container component

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MyOrders from '../components/MyOrders'
+
+const MainContainer = () => {
+    return (
+        <div className="MainContainer">
+            <MyOrders/>
+
+        </div>
+    )
+}
+
+export default MainContainer
