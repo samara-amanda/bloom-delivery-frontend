5a4bd0ac4fd2a4ada885be161e2d7e90124dd475
Sam <samara.amanda01@gmail.com>
2021-06-28 22:57:03 -0400

    clean up code relocated store

---
+++ src/store.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import usersReducer from './reducers/usersReducer'
+import currentUser from './reducers/currentUser'
+import loginForm from './reducers/loginForm'
+import myOrders from './reducers/myOrders'
+import signupForm from './reducers/signupForm.js'
+import thunk from 'redux-thunk'
+
+const reducer = combineReducers({
+    users: usersReducer, 
+    currentUser, 
+    loginForm,
+    myOrders, 
+    signupForm
+})
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
+  
