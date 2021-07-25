be0755c5181cc24ecbd6e9eb8605bca02496f28d
Sam <samara.amanda01@gmail.com>
2021-07-25 18:00:15 -0400

    import browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './store.js'
 
@@ -10,7 +11,9 @@ ReactDOM.render(
   /// we wrap APP in Provider so any child component has access to the store
 
   <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </Provider>,
 document.getElementById('root'));
  
