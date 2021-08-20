d0f2ed337c6bd9b9b5b8934e4c5c9a0d1770da1d
Sam <samara.amanda01@gmail.com>
2021-08-19 23:50:09 -0400

    separation of concern/clean up code into containers

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
 import React from 'react';
 import './App.css'
-import { connect } from 'react-redux'
-import { getCurrentUser } from './actions/currentUser.js'
-import NavBar from './components/NavBar.js'
-import Login from './components/Login.js'
-import Logout from './components/Logout.js'
-import Signup from './components/Signup.js';
 import MyOrders from './components/MyOrders.js';
-import AllItems from './components/AllItems.js'
-import Home from './components/Home.js'
+import ShopContainer from './containers/ShopContainer';
 import { Route, Switch } from 'react-router'
+import MainContainer from './containers/MainContainer';
+
 
 class App extends React.Component {
-
-  componentDidMount() {
-    this.props.getCurrentUser()
-  }
-
   render() {
-    const {loggedIn} = this.props
-      return (
+    return (
       
-        <div className="App">
-          { loggedIn ? <Logout/> : null}
-          <NavBar/>
-            <Switch>
-              <Route exact path='/' render={() => loggedIn ? <MyOrders/> : <Home/>}/>
-              <Route exact path='/signup' component={Signup}/>
-              <Route exact path='/login' component={Login}/>
-              <Route exact path='/my-orders' component={MyOrders}/>
-              <Route exact path='/shop' component={AllItems}/>
-            </Switch>
-          
-        </div>
-      );
+      <div className="App">
+          <MainContainer/>
+          <ShopContainer/>
+      
+          <Switch>
+            <Route exact path='/my-orders' component={MyOrders}/>
+          </Switch>
+      </div>
+    );
   }
 }
 
-//I do this because I know the incoming argument is an object, state, coming from redux
-// and I know it has a property called currentUser
-//state = { ...
-//  currentUser: {...}
-const mapStateToProps = state => {
-  return ({
-    loggedIn: !!state.currentUser
-  })
-}
-
-export default connect(mapStateToProps, { getCurrentUser })(App);
+export default App
