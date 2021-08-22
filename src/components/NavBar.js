d013f76bf696bcfa2a6db675f1d2a8fc4f6c6743
Sam <samara.amanda01@gmail.com>
2021-08-22 16:57:57 -0400

    nav bar setup, logged in/out ternary for nav links displayed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Logout from '../components/Logout'
 
 const NavBar = ({ currentUser }) => {
     return (
-        <div className="nav">
-            <h1>Bloom Delivery</h1>
-            { currentUser ? <h3>Welcome, {currentUser.attributes.name}</h3> : "" }
+        <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper red lighten-3">
+                <Link to="/" class="center brand-logo">Bloom Delivery</Link>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li>{ currentUser ? <>Welcome, {currentUser.attributes.name}</> : <Link to="signup">Signup</Link>}</li>
+                    <li>{ !currentUser ?  <Link to="login" >Login</Link> : null}</li>
+                    <li>{ currentUser ? <Link to="/my-orders">My Orders</Link> : null }</li>
+                    <li><Link to="/shop">Shop</Link></li>
+                    <li><a href="sass.html">About</a></li>
+                    <li>{currentUser ? <Logout/> : null }</li>
+                </ul>
+                
+            </div>
+        </nav>
         </div>
     )
 }
