59803ea6852654267505f045168a3e737fd715d3
Sam <samara.amanda01@gmail.com>
2021-08-22 16:56:51 -0400

    materialize logout button styling

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser.js'
 
 const Logout = ({ logout }) => {
-
     return (
         <form onSubmit={logout}>
-            <input type="submit" value="Log Out" />
+            <a class="waves-effect grey lighten-1 btn"><input type="submit" value="Logout" /></a>
         </form>
+        
     )
 }
 
-
 export default connect(null, { logout })(Logout)
+
+ 
