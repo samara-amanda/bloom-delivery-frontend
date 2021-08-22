7dd009bb3b577001fe97d392c99f4c0d2ba72a24
Sam <samara.amanda01@gmail.com>
2021-08-22 16:55:21 -0400

    homepage image banner, map state to props for currentUser

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Home = ({ currentUser }) => (
-    <div>
-        { currentUser ? "" : <h3><Link to="/signup">Sign Up</Link> OR <Link to="/login"> Login</Link></h3> }
-    </div>
-);
+const Home = ({ currentUser }) => {
+    return (
+        <div>
+            <center>
+            <img class="materialboxed " src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/31841937_926683800824343_2841788991416565760_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HaSd4_MP2J8AX9z_BWx&_nc_ht=scontent-lga3-1.xx&oh=36566cdf96b807ab3eaf37a351e36508&oe=6146513F" width="1200" height="800" />
+            </center>
+        </div>
+    )
+}
 
-export default Home;
+const mapStateToProps = ({ currentUser }) => {
+    return {
+        currentUser
+    }
+}
+
+export default connect(mapStateToProps)(Home);
