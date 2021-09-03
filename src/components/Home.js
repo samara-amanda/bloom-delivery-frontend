ee087055419e924143f2893f79eb9b888bdac0b6
Sam <samara.amanda01@gmail.com>
2021-09-02 21:30:22 -0400

    materialize styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = ({ currentUser }) => {
     return (
         <div>
             <center>
-            <img class="materialboxed " src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/31841937_926683800824343_2841788991416565760_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HaSd4_MP2J8AX9z_BWx&_nc_ht=scontent-lga3-1.xx&oh=36566cdf96b807ab3eaf37a351e36508&oe=6146513F" width="1200" height="800" />
+            <img class="materialboxed " src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/31841937_926683800824343_2841788991416565760_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=e3f864&_nc_ohc=HaSd4_MP2J8AX9z_BWx&_nc_ht=scontent-lga3-1.xx&oh=36566cdf96b807ab3eaf37a351e36508&oe=6146513F" width="1300" height="750" />
             </center>
         </div>
     )
--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -3,28 +3,36 @@ import { Link } from 'react-router-dom'
 
 const ShopCard = ({ items }) => {
     return (
-        <div className="shop-list">
-            {items.map(i => 
-        <div key={i.id} name={"shop-card-" + i.id}>
-            <ul>
-                <li>
+        
+            <div class="shop">
+                
                 <center>
-                    <h3>
-                        <Link to="/item">
-                            {i.attributes.name}
-                        </Link>
-                    </h3>
-                    <img src={i.attributes.image} height="200" width="200" alt="item" />
-                    <br></br>
-                    <h4>
-                        ${i.attributes.price}
-                    </h4>
-                </center>
-                </li>
-            </ul>
-        </div>
-    )}
-        </div>
+                <img class="materialboxed" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/maison-0311-vef1574-1590173699.jpg" width="1300" height="800"/>
+
+                <div class="caption center-align">
+                    <h5 class="light grey-text text-ighten-3 hide-on-small-only">Shop all of our new favorites!</h5>
+                </div>
+                
+                    <div class="row">
+                        <div class="col s12 m4">
+                            
+                            {items.map(i =>
+                                <div class="card" key={i.id} name={"shop-card-" + i.id}>
+                                    <div class="card-image">
+                                        <img class="activator" src={i.attributes.image} alt="item" />
+                                        <br></br>
+                                        <span class="card-title"><Link to="/item">{i.attributes.name}</Link></span>
+                                    </div>
+                                    <div class="card-content grey lighten-4">
+                                        <p>${i.attributes.price}.00</p>      
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    </center>
+            </div>
+
     )
 }
 
