0848427b1a53d00df364eea6bc0d9700a5045b69
Sam <samara.amanda01@gmail.com>
2021-09-16 15:50:09 -0400

    nav bar link alignment change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Logout from '../components/Logout'
+import Home from '../components/Home'
 
 const NavBar = ({ currentUser }) => {
     return (
+        <div>
         <div class="navbar-fixed">
-        <nav>
-            <div class="nav-wrapper red lighten-3">
-                <Link to="/" class="center brand-logo">Bloom Delivery</Link>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li>{ currentUser ? <>Welcome, {currentUser.attributes.name}</> : <Link to="signup">Signup</Link>}</li>
-                    <li>{ !currentUser ?  <Link to="login" >Login</Link> : null}</li>
-                    <li>{ currentUser ? <Link to="/my-orders">My Orders</Link> : null }</li>
-                    <li><Link to="/shop">Shop</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li>{currentUser ? <Logout/> : null }</li>
-                </ul>
-                
-            </div>
-        </nav>
+            <nav>
+                <div class="nav-wrapper red lighten-3">
+                    <a href="/" class="center brand-logo">Bloom Delivery</a>
+                    <ul id="nav-mobile" class="left hide-on-med-and-down">
+                        <li>{ currentUser ? <>Welcome, {currentUser.attributes.name}</> : <a href="signup">Signup</a>}</li>
+                        <li>{ !currentUser ?  <a href="login">Login</a> : null}</li>
+                        <li>{ currentUser ? <a href="/my-orders">My Orders</a> : null }</li>
+                        <li><a href="/shop">Shop</a></li>
+                        <li><a href="/about">About</a></li>
+                    </ul>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li>{currentUser ? <Logout/> : null }</li>
+                    </ul>
+                    
+                </div>
+            </nav>
         </div>
+        </div>
+        
     )
 }
 
