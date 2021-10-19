06db61fd4178f5d32297827acc3a97f686dec2ca
Sam <samara.amanda01@gmail.com>
2021-10-19 19:29:49 -0400

    fetch url updates:

--- src/actions/fetchItems.js
+++ src/actions/fetchItems.js
@@ -1,6 +1,6 @@
 export function fetchItems() {
     return (dispatch) => {
-        fetch("https://bloom-delivery.herokuapp.com/api/v1/items")
+        fetch("https://bloom-delivery-api.herokuapp.com/api/v1/items")
         .then (resp => resp.json())
         .then(items => dispatch({
             type: 'FETCH_ITEMS',
--- src/actions/manageOrders.js
+++ src/actions/manageOrders.js
@@ -19,7 +19,7 @@
 //asynchronous actions
 export const fetchOrders = () => {
     return dispatch => {
-        return fetch("https://bloom-delivery.herokuapp.com/api/v1/orders", {
+        return fetch("https://bloom-delivery-api.herokuapp.com/api/v1/orders", {
             credentials: "include",
             method: "GET", 
             headers: {
