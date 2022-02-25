9dd40db3deb6344743fe0f924f52792d85ad55bd
Sam <samara.amanda01@gmail.com>
2022-02-25 11:07:55 -0500

    import updates, renamed class to className per jsx standards, refactored logout form handler

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { updateLoginForm } from '../actions/loginForm.js'
-import { login } from '../actions/currentUser.js'
+import { login, updateLoginForm } from '../actions/currentUser'
 
 const Login = ({ loginForm, updateLoginForm, login }) => {
     
@@ -21,18 +20,20 @@ const Login = ({ loginForm, updateLoginForm, login }) => {
 
     return (
 
-        <div class="row">
-            <form class="col s12" onSubmit={handleSubmit}>
-                <div class="row">
-                    <div class="input-field col s6">
-                        <input id="email" type="text" class="validate" name="email" value={loginForm.email} onChange={handleInputChange}/>
+        <div className="row">
+            <form className="col s12" onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="input-field col s6">
+                        <input id="email" type="text" className="validate" name="email" value={loginForm.email} onChange={handleInputChange}/>
                         <label for="email">Email</label>
                     </div>
-                    <div class="input-field col s6">
-                        <input id="password" type="password" class="validate" name="password" value={loginForm.password} onChange={handleInputChange}/>
+
+                    <div className="input-field col s6">
+                        <input id="password" type="password" className="validate" name="password" value={loginForm.password} onChange={handleInputChange}/>
                         <label for="password">Password</label>
                     </div>
-                    <a href="" class="waves-effect pink darken-1 btn"><input type="submit" value="Login" /></a>
+                    
+                    <input type="submit" value="Login" className="waves-effect pink darken-1 btn"/>
                 </div>
             </form>
         </div>
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { logout } from '../actions/currentUser.js'
+import { logout } from '../actions/currentUser'
+import { withRouter } from 'react-router-dom'
 
-const Logout = ({ logout }) => {
+const Logout = ({ logout, history }) => {
     return (
-        <form onSubmit={logout}>
-            <a class="waves-effect waves-teal btn-flat"><input class="grey-text text-lighten-3 " type="submit" value="Logout" /></a>
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            logout()
+            history.push('/')
+        }}>
+            
+            <input className="waves-effect waves-teal btn-flat grey-text text-lighten-3 " type="submit" value="Logout" />
         </form>
         
     )
 }
 
-export default connect(null, { logout })(Logout)
+export default withRouter(connect(null, { logout })(Logout))
 
  
