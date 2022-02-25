1b8eea7f6ae049188929668660c2845f354e6d01
Sam <samara.amanda01@gmail.com>
2022-02-25 10:57:14 -0500

    moved store to index.js file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
-import store from './store.js'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import currentUser from './reducers/currentUser'
+import loginForm from './reducers/loginForm'
+import myOrders from './reducers/myOrders'
+import signupForm from './reducers/signupForm.js'
+import allItems from './reducers/allItems.js'
+
+const reducer = combineReducers({ 
+  currentUser, 
+  loginForm,
+  myOrders, 
+  signupForm,
+  allItems
+})
 
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(
+  reducer, 
+  composeEnhancer(applyMiddleware(thunk))
+)
 
 ReactDOM.render(
   /// we wrap APP in Provider so any child component has access to the store
