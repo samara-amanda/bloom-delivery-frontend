6326a1dd51c6c2d7f7ff5a5342f42b3f7b6533db
Sam <samara.amanda01@gmail.com>
2022-02-25 10:11:44 -0500

    fetch request url changes

--- src/actions/fetchItems.js
+++ src/actions/fetchItems.js
@@ -1,6 +1,6 @@
 export function fetchItems() {
     return (dispatch) => {
-        fetch("http://bloom-delivery-api.herokuapp.com/api/v1/items")
+        fetch("https://bloom-delivery-api.herokuapp.com/api/v1/items")
         .then (resp => resp.json())
         .then(items => dispatch({
             type: 'FETCH_ITEMS',
